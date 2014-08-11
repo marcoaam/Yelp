9cb88018a80926a6a3cab8152b1d843868d492cb
marcoaam <marco.army@gmail.com>
2014-08-11 12:29:04 +0100

    Restaurant added test passed

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
-feature 'Restaurant' do
+feature 'Without restaurants' do
 	scenario 'No Restaurants have been added' do
 		visit '/restaurants'
 		expect(page).to have_content 'No restaurants have been added'
 		expect(page).to have_link 'Add Restaurant'
 	end
+end
+
+feature 'With restaurants' do
+
+	before(:each) do
+		Restaurant.create(name: 'Burger King')
+	end
+
+	scenario 'Restaurants have been added' do
+		visit '/restaurants'
+		expect(page).not_to have_content 'No restaurants have been added'
+		expect(page).to have_content 'Burger King'
+		expect(page).to have_link 'Add Restaurant'
+	end
+	
 end
